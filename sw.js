d6360a67788fea29bec6fffb5c6258276ea334df
Lonny Sakalese <lonnysakalesepro@gmail.com>
2026-02-16 18:46:57 +0000

    bump SW cache to v9

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // --- CONFIGURATION DU CACHE ---
 
 // Nom du cache. Changer cette valeur invalidera le cache existant et en créera un nouveau.
-const CACHE_NAME = 'warrior-tracker-v8';
+const CACHE_NAME = 'warrior-tracker-v9';
 
 // Liste des fichiers essentiels à mettre en cache pour que l'application fonctionne hors ligne.
 const urlsToCache = [
